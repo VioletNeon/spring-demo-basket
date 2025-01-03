6c6ed20deb8e00c8aad72ac7675282dcfc61173c
Куликов Валерий Романович <VRKulikov@beeline.ru>
2025-01-03 17:03:03 +0300

    Initial commit

---
+++ src/main/java/violet/neon/springdemobasket/SpringDemoBasketApplication.java
@@ -0,0 +1,13 @@
+package violet.neon.springdemobasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoBasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/violet/neon/springdemobasket/SpringDemoBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package violet.neon.springdemobasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
